8056ca9cc5591e215502b60e552763311bd7e7dd
Hopdd <hopdaoduy@vccorp.vn>
2022-03-07 14:47:06 +0700

    firsst commit

---
+++ test/src/main/java/com/example/test/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.example.test.activity;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    public final String TAG = getClass().getSimpleName();
+}
